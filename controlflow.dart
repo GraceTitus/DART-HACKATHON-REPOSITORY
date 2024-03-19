b2b9afe18284e547fd541146e4f2e8500ea9f9c3
GraceTitus <133824320+GraceTitus@users.noreply.github.com>
2024-03-19 20:18:32 +0300

    Update controlflow.dart

--- controlflow.dart
+++ controlflow.dart
@@ -5,3 +5,19 @@ Write a Dart program to determine the grade based on a student's marks. The prog
 - If the marks are between 75 and 85 (inclusive), print "Very Good".
 - If the marks are between 65 and 75 (inclusive), print "Good".
 - If the marks are below 65, print "Average".
+void main() {
+  int marks = 75;
+  print("Your grade is ${getGrade(marks)}");
+}
+
+String getGrade(int marks) {
+  if (marks > 85) {
+    return "Excellent";
+  } else if (marks >= 75 && marks <= 85) {
+    return "Very Good";
+  } else if (marks >= 65 && marks < 75) {
+    return "Good";
+  } else {
+    return "Average";
+  }
+}
