c44f6b2d36744a2cbcf286191be6ce03849d7e8f
GraceTitus <133824320+GraceTitus@users.noreply.github.com>
2024-03-19 20:22:28 +0300

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -1,2 +1,14 @@
 ## Program 2: Perform Mathematical Operations with Functions
 Write a Dart program that performs two mathematical operations using functions.
+void main() {
+  print("Sum of 5 and 3: ${sum(5, 3)}");
+  print("Difference between 10 and 4: ${difference(10, 4)}");
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int difference(int a, int b) {
+  return a - b;
+}
